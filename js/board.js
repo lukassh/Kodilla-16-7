5b0a0816fbdd82590961138cc63c5bcce58928b8
Lukass <lukassh@gmail.com>
2019-03-04 19:23:58 +0100

    Modify scripts files

---
+++ js/board.js
@@ -0,0 +1,42 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '3878',
+  'X-Auth-Token': 'a0b90e630fd1303a299243445e158e1c'
+};
+
+var board = {
+    name: 'Tablica Kanban',
+    addColumn: function(column) {
+      this.element.appendChild(column.element);
+      initSortable(column.id); //About this feature we will tell later
+    },
+    element: document.querySelector('#board .column-container')
+};
+
+document.querySelector('#board .create-column').addEventListener('click', function() {
+  var name = prompt('Enter a column name');
+  var data = new FormData();
+
+  data.append('name', name);
+
+  fetch(baseUrl + '/column', {
+      method: 'POST',
+      headers: myHeaders,
+      body: data,
+    })
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(resp) {
+      var column = new Column(resp.id, name);
+      board.addColumn(column);
+    });
+});
+
+function initSortable(id) {
+  	var el = document.getElementById(id);
+  	var sortable = Sortable.create(el, {
+    	group: 'kanban',
+    	sort: true
+  	});
+}
